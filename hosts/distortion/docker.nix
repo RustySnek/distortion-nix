d97c0493e8f8f878a69965dfde47cd7eb83955a3
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-06-02 07:19:54 +0200

    docker_25

--- hosts/distortion/docker.nix
+++ hosts/distortion/docker.nix
@@ -3,9 +3,9 @@
   environment.systemPackages = with pkgs; [
     docker-compose
     nvidia-container-toolkit
-    docker
   ];
   virtualisation.docker.enable = true;
+  virtualisation.docker.package = pkgs.docker_25;
   virtualisation.docker.rootless = {
     enable = false;
     setSocketVariable = true;
