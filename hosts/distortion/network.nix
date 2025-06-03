bc5e19652c8bc226a913aacaa4041ed0d1bd9239
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-06-03 19:24:39 +0200

    setup caddy

--- hosts/distortion/network.nix
+++ hosts/distortion/network.nix
@@ -1,3 +1,18 @@
-{...}: {
+{ ... }:
+{
   networking.useDHCP = true;
+  services.caddy = {
+    enable = true;
+    virtualHosts."localhost".extraConfig = ''
+      :1337
+
+      handle_path /comfy/* {
+          reverse_proxy localhost:7860
+      }
+
+      handle_path /chat/* {
+          reverse_proxy localhost:7861
+      }
+    '';
+  };
 }
