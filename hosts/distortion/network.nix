654cfb087de987e0198a729f6f5233413ffcc98c
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-06-03 22:00:30 +0200

    fix pathing

--- hosts/distortion/network.nix
+++ hosts/distortion/network.nix
@@ -5,15 +5,17 @@
   services.caddy = {
     enable = true;
     virtualHosts."http://localhost:6969".extraConfig = ''
-      handle_path /comfy* {
+      handle_path /comfy {
           reverse_proxy localhost:7860
       }
 
-      handle_path /chat* {
+      redir /chat /chat/
+      handle_path /chat/ {
           reverse_proxy localhost:7861
       }
 
-      respond "Grim"
+
+      reverse_proxy localhost:7861
     '';
   };
 }
